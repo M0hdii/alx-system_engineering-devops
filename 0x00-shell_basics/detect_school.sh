dad1c064daf0e4fadd1c0f21d13f396b0d7a977d
M0hdii <mehdiomar71@gmail.com>
2023-08-02 13:02:27 -0700

    school.mgc

--- 0x00-shell_basics/detect_school.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in "$@"; do
-  if [[ $(head -c 6 "$file") == "SCHOOL" ]]; then
-    echo "$file: School data"
-  else
-    echo "$file: $(file -b --mime-type "$file")"
-  fi
-done
